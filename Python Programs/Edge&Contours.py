cb22d7b6856843b9a403a437cb20ebd9bd2e3c49
Lloyd Mundo <lloyddenver@gmail.com>
2019-04-11 09:18:52 +0100

    added in function to find contours and a function to draw them on the immage

--- Python Programs/Edge&Contours.py
+++ Python Programs/Edge&Contours.py
@@ -20,12 +20,18 @@ B = cv2.adaptiveThreshold(G, maxValue=255,
         thresholdType = cv2.THRESH_BINARY,
         blockSize = 5, C = 15)
 
+Im_copy = B
 
 #Find contours
+contours, hierarchy = cv2.findContours(E,
+                mode = cv2.RETR_EXTERNAL,
+                method = cv2.CHAIN_APPROX_SIMPLE)
 
-
+#draw contours
+Im_copy = cv2.drawContours(Im_copy, contours, contourIdx=-1, color=(0,0,255), thickness=2)
 
 plt.subplot(2,2,1),plt.imshow(G, cmap='gray')
 plt.subplot(2,2,2),plt.imshow(E, cmap='gray')
 plt.subplot(2,2,3),plt.imshow(B, cmap='gray')
+plt.subplot(2,2,4),plt.imshow(Im_copy, cmap='gray')
 plt.show()
