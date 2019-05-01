1c17eb078c71cff9e962ac6aae6f186aae0be157
Lloyd Mundo <lloyddenver@gmail.com>
2019-05-01 21:17:14 +0100

    added pseudo code for a command to send on Arduino

--- Python Programs/ColourDetection.py
+++ Python Programs/ColourDetection.py
@@ -9,47 +9,72 @@ import numpy as np
 img = cv2.imread("testImage.png",1)
 imgCopy = img.copy()
 
+#Determine the size of the image
+imgHeight = np.size(imgCopy,0)
+imgWidth = np.size(imgCopy, 1)
+xCenter = int(imgWidth/2)
+yCenter = int(imgHeight/2)
+xLimitLeft = xCenter - int(0.40*imgWidth)
+xLimitRight = xCenter + int(0.40*imgWidth)
+yLimit = int(0.25*imgHeight)
+
 #Convert img frame from BGR to HSV
 imgHSV = cv2.cvtColor(imgCopy, cv2.COLOR_BGR2HSV)
 
-#defining the range of green color
+#deflining the range of green color
+woodLower = np.array([9,16,163], np.uint8)
+woodUpper = np.array([29,36,243], np.uint8)
 greenLower = np.array([22,80,40],np.uint8)
 greenUpper = np.array([60,255,255],np.uint8)
 
 #Filter out other colours, only show green colour
 greenMask = cv2.inRange(imgHSV, greenLower, greenUpper)
 
+#Wood mask
+woodMask = cv2.inRange(imgHSV, woodLower, woodUpper)
+
 #Morphological Transformation
 kernelOpen = np.ones((8,8),"uint8")
-kernelClose = np.ones((20,20),"uint8")
-maskOpenMorph = cv2.morphologyEx(greenMask, cv2.MORPH_OPEN, kernelOpen)
-maskCloseMorph = cv2.morphologyEx(greenMask, cv2.MORPH_CLOSE, kernelClose)
+kernelClose = np.ones((15,15),"uint8")
+kernelOpen2 = np.ones((8,8),"uint8")
+maskOpenMorph = cv2.morphologyEx(greenMask | woodMask, cv2.MORPH_OPEN, kernelOpen)
+maskCloseMorph = cv2.morphologyEx(greenMask | woodMask, cv2.MORPH_CLOSE, kernelClose)
+maskOpenMorph2 = cv2.morphologyEx(maskCloseMorph, cv2.MORPH_OPEN, kernelOpen2)
 
 #Find Contours 
-contours,hierarchy = cv2.findContours(maskCloseMorph.copy(),
+contours,hierarchy = cv2.findContours(maskOpenMorph2.copy(),
                          mode = cv2.RETR_EXTERNAL,
                          method = cv2.CHAIN_APPROX_NONE)
 
 #cv2.drawContours(imgCopy, contours, -1, (0,0,255), thickness = 2)
 
+#Bound the contours if contours are large enough
 for i in range(len(contours)):
      area = cv2.contourArea(contours[i])
-     if(area > 300):
+     if(area > 250):
           x,y,w,h = cv2.boundingRect(contours[i])
           cv2.rectangle(imgCopy, (x,y), (x+w, y+h), (255,0,0), 2)
-          print("(" + str(x) + "," + str(y) + ")    (" + str(x+w) + "," + str(y+h) + ")")
+
+''' pseudo code for arduino command         
+          if(y >= yLimit):
+               if(((x>xLimitLeft) || (x+w > xLimitLeft)) && ((x<xCenter) || (x+w < xCenter))):
+                    # tell arduino to make a slight turn to right
+               else if(((x<xLimitRight) || (x+w < xLimitRight)) && ((x>xCenter) || (x+w > xCenter))):
+                    # tell arduino to make a slight turn to left
+'''         
+
+cv2.line(imgCopy, (0,yLimit), (imgWidth,yLimit), (255,255,255), 2) # top limit
+cv2.line(imgCopy, (xLimitLeft, 0), (xLimitLeft, imgHeight), (255,255,255), 2) # left limit
+cv2.line(imgCopy, (xLimitRight,0), (xLimitRight, imgHeight), (255,255,255), 2) # right limit
 
 
-imgHeight = np.size(imgCopy,0)
-imgWidth = np.size(imgCopy, 1)
-print("H = "+str(imgHeight) + "\nWidth = " + str(imgWidth))
 cv2.imshow("Image in test", imgCopy)
-cv2.imshow("mask", greenMask)
+cv2.imshow("mask", greenMask | woodMask)
 cv2.imshow("Morphology Opening", maskOpenMorph)
-cv2.imshow("Morphology Closing", maskCloseMorph)
+cv2.imshow("Morphology second Opening", maskOpenMorph2)
 cv2.waitKey(0)
 
-
+#print("H = "+str(imgHeight) + "\nWidth = " + str(imgWidth))
 
 '''
 #Tracking Colour (Yellow) 
@@ -61,7 +86,4 @@ cv2.waitKey(0)
                    x,y,w,h = cv2.boundingRect(contour)     
                         img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
 
-#Display results
-img = cv2.flip(img,1)
-cv2.imshow("Yellow",res)
 '''
