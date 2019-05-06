23d31b1b359c34a0e10f10720042bdb0c9ca5ccb
Lloyd Mundo <lloyddenver@gmail.com>
2019-05-06 15:22:58 +0100

    test is changed to turn on/off an led when a reply from pi arrives

--- Arduino Programs/SerialTest/SerialTest.ino
+++ Arduino Programs/SerialTest/SerialTest.ino
@@ -1,20 +1,18 @@
-
 String messageReceived;
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT); //built in LED
+  Serial.begin(9600);   //baud rate
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.write("Arduino Message");
-  if(Serial.available() > 0)
+  Serial.write("Hello");  //writes Hello into serial buffer
+  while(Serial.available() == 0); //waits for data in buffer
+  messageReceived = Serial.readString();  // reads the buffer and decodes it to string
+  if (messageReceived == "rpi") //check if the reply from pi is rpi
   {
-    messageReceived = Serial.read();
-    Serial.println(messageReceived);
+    digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));  //turn ON/OFF the LED
   }
-  else
-    Serial.println("error\n");
-
-  delay(2000);
+  messageReceived = ""; //flushes the reply
+  delay(1000); //One second delay
 }
