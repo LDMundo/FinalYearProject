07d5ad4aa91551cac3e80d362b1a71d2b9f4c720
LDMundo <lloyddenver@gmail.com>
2019-05-06 13:05:32 +0100

    changed to correct port and test encoding and decoding messages

--- Python Programs/TestSerialComm.py
+++ Python Programs/TestSerialComm.py
@@ -1,12 +1,17 @@
 import serial
-ser = serial.Serial(‘/dev/ttyAMA0’, 9600)
+ser = serial.Serial(
+        port='/dev/serial0',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=0.3
+)
+
 
 while True:
-    ser.open()
     while ser.inWaiting():
-        receivedMessage = ser.readline()
-        print(receivedMessage + "\n")
-        ser.write("message received")
-
+        response = ser.read(8)
+        print('message received: ' + response.decode('utf-8'))
+        ser.write('received'.encode('utf-8'))
     
-
