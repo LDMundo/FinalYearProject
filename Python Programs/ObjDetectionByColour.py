7a1154a261967a5ee1a5a0dc9ddbc01eba85c037
LDMundo <lloyddenver@gmail.com>
2019-05-07 14:58:08 +0100

    changed the algorithm

--- Python Programs/ObjDetectionByColour.py
+++ Python Programs/ObjDetectionByColour.py
@@ -22,8 +22,8 @@ ser = serial.Serial(
         timeout=0.3                     #timeout
 )
 
-#Global Variables
-#cam = cv2.VideoCapture(0)
+#camera
+cam = cv2.VideoCapture(0)
 
 #function to return mask 
 def extractMask(img):
@@ -34,8 +34,8 @@ def extractMask(img):
     #deflining the range of colours [H, S, V]
     woodLower = np.array([9, 16, 163], np.uint8)
     woodUpper = np.array([29, 36, 243], np.uint8)
-    greenLower = np.array([22, 80, 40], np.uint8)
-    greenUpper = np.array([60, 255, 255], np.uint8)
+    greenLower = np.array([22, 26, 40], np.uint8)
+    greenUpper = np.array([99, 255, 255], np.uint8)
 
     #Filter out other colours, only show green colour
     greenMask = cv2.inRange(imgHSV, greenLower, greenUpper)
@@ -53,13 +53,16 @@ def extractMask(img):
 
 while True:
     while ser.inWaiting():
-        message = ser.read(5)
-        if (message.decode('utf-8') == "Hello"):
-            #_, frame = cam.read()
-            frame = cv2.imread("testImage.JPG",1)
+        message = ser.read(7)
+        print(message.decode('utf-8') + "\n")
+        if (message.decode('utf-8') == "request"):
+            _, frame = cam.read()
+            #frameCopy = cv2.imread("testImage.JPG",1)
+            frameCopy = frame.copy()
+                        
             #Determine the size of the image
-            imgHeight = np.size(frame, 0)
-            imgWidth = np.size(frame, 1)
+            imgHeight = np.size(frameCopy, 0)
+            imgWidth = np.size(frameCopy, 1)
 
             #Limits 
             xCenter = int(imgWidth/2)
@@ -68,34 +71,47 @@ while True:
             xLimitRight = xCenter + int(0.40*imgWidth)
             yLimit = int(0.25*imgHeight)
 
-            mask = extractMask(frame)
-            _, contours, _= cv2.findContours(mask.copy(), mode = cv2.RETR_EXTERNAL, method = cv2.CHAIN_APPROX_NONE)
+            mask = extractMask(frame.copy())
+            _, contours, _= cv2.findContours(mask, mode = cv2.RETR_EXTERNAL, method = cv2.CHAIN_APPROX_NONE) #extract contours
+            closestYplusH = 0
+            xOfInterest = 0
+            yOfInterest = 0
+            wOfInterest = 0
+            hOfInterest = 0            
             for i in range(len(contours)):
                 area = cv2.contourArea(contours[i])
                 if(area > 250):
                     x,y,w,h = cv2.boundingRect(contours[i])
-                    cv2.rectangle(mask.copy(), (x,y), (x+w, y+h), (255,0,0), 2)
-                    xplusw = x + w
-                    print("rectangle " + str(i) + "   " + str(x) + "," + str(y))
+                    cv2.rectangle(frameCopy, (x,y), (x+w, y+h), (255,0,0), 2)
+                    #print("rectangle " + str(i) + "   " + str(x) + "," + str(y))
+                    
+                    if y+h > closestYplusH:
+                        closestYplusH = y+h
+                        xOfInterest = x
+                        yOfInterest = y
+                        wOfInterest = w
+                        hOfInterest = h 
 
-                    if y+h >= yLimit:
-                        if (x>xLimitLeft or xplusw>xLimitLeft) and (x<xCenter or xplusw<xCenter):
-                            print("turnRight\n")
-                            #ser.write('turnRight'.encode('utf-8'))
-                            #ser.flush() 
+            if closestYplusH > 0:
+                if closestYplusH >= yLimit:
+                    xplusw = xOfInterest + wOfInterest
+                    if (xOfInterest>xLimitLeft or xplusw>xLimitLeft) and (xOfInterest<xCenter or xplusw<xCenter):
+                        print("turnRight\n")
+                        #ser.write('turnRight'.encode('utf-8'))
+                        #ser.flush() 
                             
-                        elif (x<xLimitRight or xplusw<xLimitRight) and (x>xCenter or xplusw>xCenter):
-                            print("turnLeft\n")
-                            #ser.write('turnLeft'.encode('utf-8'))
-                            #ser.flush()  
-                    elif (y+h <= yLimit) and (y+h >= yLimit-int(0.5*yLimit)):
+                    elif (xOfInterest<xLimitRight or xplusw<xLimitRight) and (xOfInterest>xCenter or xplusw>xCenter):
+                        print("turnLeft\n")
+                        #ser.write('turnLeft'.encode('utf-8'))
+                        #ser.flush()  
+                elif (closestYplusH <= yLimit) and (closestYplusH >= yLimit-int(0.5*yLimit)):
                         print("reverse\n")
                         #ser.write('reverse'.encode('utf-8'))
                         #ser.flush() 
-                else:
-                    print("noObject\n")
-                    #ser.write('noObject'.encode('utf-8'))
-                    #ser.flush() 
-
+            else:
+                print("noObject\n")
+                ser.write('noObject'.encode('utf-8'))
+                ser.flush() 
+        
 
 
